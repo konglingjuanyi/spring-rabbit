1b159bd7ce5ac8b5e5954a6af5383108ab7c31e0
Sandokan <sandokandias@gmail.com>
2016-03-30 10:39:29 -0300

    jira #DC-92 - extracting rabbit producer

---
+++ spring-rabbit/src/main/java/br/com/zup/spring/rabbit/producer/RabbitQueueProducer.java
@@ -0,0 +1,33 @@
+package br.com.zup.spring.rabbit.producer;
+
+import br.com.zup.spring.rabbit.infra.QDelivery;
+import br.com.zup.spring.rabbit.manager.RabbitManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class RabbitQueueProducer implements QueueProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RabbitQueueProducer.class);
+
+    private final RabbitManager rabbitManager;
+
+    public RabbitQueueProducer(RabbitManager rabbitManager) {
+        this.rabbitManager = rabbitManager;
+    }
+
+    @Override
+    public CompletableFuture<QDelivery.DeliveryStatus> publish(QDelivery delivery) {
+
+        return CompletableFuture.supplyAsync(() -> {
+
+            LOG.debug("Sending message [{}] to [{}] ...", delivery.message.toString(), delivery.queue.name);
+
+            rabbitManager.publish(delivery);
+
+            return QDelivery.DeliveryStatus.SENT;
+        });
+    }
+
+}
