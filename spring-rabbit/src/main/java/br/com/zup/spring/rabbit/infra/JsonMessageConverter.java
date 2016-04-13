bf72ad5a47c91e65df542d6e2702c4546ed8af90
Sandokan <sandokandias@gmail.com>
2016-04-13 12:03:00 -0300

    jira #DC-136 - removing unecessary folder

--- spring-rabbit/src/main/java/br/com/zup/spring/rabbit/infra/JsonMessageConverter.java
+++
@@ -1,9 +0,0 @@
-package br.com.zup.spring.rabbit.infra;
-
-public final class JsonMessageConverter implements MessageConverter<JsonMessage> {
-
-    public JsonMessage convert(byte[] body) {
-
-        return new JsonMessage(body);
-    }
-}
--- spring-rabbit/src/main/java/br/com/zup/spring/rabbit/infra/MessageConverter.java
+++
@@ -1,6 +0,0 @@
-package br.com.zup.spring.rabbit.infra;
-
-public interface MessageConverter<T extends QMessage> {
-
-    T convert(byte[] body);
-}
